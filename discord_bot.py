b39a77aebe2b2808a9527df93bd9e1e851db6b70
xinaiderf <bielschneider74@gmail.com>
2025-08-11 01:41:40 -0400

    bot básico

--- discord_bot.py
+++ discord_bot.py
@@ -1,27 +1,11 @@
 import discord
 from discord.ext import commands
-<<<<<<< HEAD
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('token_dc')
-
-intents = discord.Intents.default()  # Intents padrões
-intents.message_content = True       # Se quiser receber conteúdo das mensagens (necessário pra ler mensagens)
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'Logado como {client.user}')
-
-client.run(TOKEN)
-=======
-
-# Coloque aqui o token do seu bot
-TOKEN = 'SEU_TOKEN_AQUI'
 
 intents = discord.Intents.default()
 intents.message_content = True  # necessário para ler mensagens
@@ -37,4 +21,3 @@ async def ping(ctx):
     await ctx.send('Pong!')
 
 bot.run(TOKEN)
->>>>>>> c16315cf543b2ef9fad561d828c6027ba7b10767
